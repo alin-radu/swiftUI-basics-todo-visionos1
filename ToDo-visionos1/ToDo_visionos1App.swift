54018f16861433ac4366d508b57f9292be295e7b
Alin RADU <alin.radu.cristian@gmail.com>
2024-02-06 14:26:13 +0200

    Initial Commit

---
+++ ToDo-visionos1/ToDo_visionos1App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_visionos1App.swift
+//  ToDo-visionos1
+//
+//  Created by Alin RADU on 06.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_visionos1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
